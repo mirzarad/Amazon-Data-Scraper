69bddd05bafd8f76afb01e145d7582bde3ec6d97
mirzarad <mirza.radoncic@stonybrook.edu>
2022-03-03 23:04:57 -0500

    Add apiKey and baseURL

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ const request = require('request-promise')
 
 const app = express()
 const PORT = process.env.PORT || 3000
+const apiKey = 'c3aa55e23c2e2a0d775d8e3c0f262b21'
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
 
 app.use(express.json());
 
 app.get('/', function(req, res){
   res.send('Welcome to my Amazon Web Scraper API')
 })
+
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
