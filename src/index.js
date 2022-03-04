44a10126d3cbe6335377586b6758e3afe8003e6c
mirzarad <mirza.radoncic@stonybrook.edu>
2022-03-04 01:22:18 -0500

    Add Get Product Reviews

--- src/index.js
+++ src/index.js
@@ -25,4 +25,16 @@ app.get('/products/:productId', async (req,res) => {
   }
 })
 
+// GET Product Reviews
+app.get('/products/:productId/reviews', async (req,res) => {
+  const { productId } = req.params
+
+  try {
+    const response = await request(`${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`)
+    res.json(JSON.parse(response))
+  } catch(error) {
+    res.json(error)
+  }
+})
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
